087eee6f9d6cf81ffaf449e96c287b5ee5042793
1211523 <128061767+1211523@users.noreply.github.com>
2024-10-26 11:54:41 +0100

    #17 #18 Staff creation
    Added parameters to class Staff, needs clarification in the diagram

--- Domain/Users/Staff.cs
+++ Domain/Users/Staff.cs
@@ -1,7 +1,15 @@
 namespace Hospital.Domain.Users
 {
-    public class Staff 
+
+    public class Staff : Entity<LicenseNumber>, IAggregateRoot
     {
-        // Add properties and methods specific to Nurse here
+        public LicenseNumber LicenseNumber { get; set; } // License number of the staff
+        public string Name { get; set; } // Full name of the staff
+        public string Specialization { get; set; } // Specialization of the staff
+        public List<string> Slots { get; set; } //to be saved like "2024-09-25:19h00/2024-09-26:02h00"
+        public SystemUser SystemUser { get; set; }  // SystemUser linked to the staff
     }
+
+    
+
 }
