5aae990282da11c8a1b2a7cb6eeeb1e6bcfdd19a
Bernardo <tradestuga@gmail.com>
2024-10-26 17:32:55 +0100

    Run comands test

--- obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13a2a45eab82601a62a2bb646ad0a5d4ea9de5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0e4de0458ccb074b8613aef51132862da053d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
