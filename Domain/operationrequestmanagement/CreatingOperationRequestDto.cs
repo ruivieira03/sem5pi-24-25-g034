cacc4363d18640514a11509d190b2a0cc75e7629
1211523 <128061767+1211523@users.noreply.github.com>
2024-10-26 20:03:50 +0100

    #20 Operation request creatingDto and changes to account for recent changes to how Id was handled

---
+++ Domain/operationrequestmanagement/CreatingOperationRequestDto.cs
@@ -0,0 +1,22 @@
+namespace Hospital.Domain.operationrequestmanagement
+{
+    public class CreatingOperationRequestDto
+    {
+        public OperationRequestId ID { get; set; }
+        public Guid PatientID { get; set; }
+        public LicenseNumber DoctorID { get; set; }
+        public string OperationTypeID { get; set; }
+        public DateTime DeadlineDate { get; set; }
+        public int Priority { get; set; }
+
+        public CreatingOperationRequestDto(OperationRequestId ID, Guid PatientID, LicenseNumber DoctorID, string OperationTypeID, DateTime DeadlineDate, int Priority)
+        {
+            this.ID = ID;
+            this.PatientID = PatientID;
+            this.DoctorID = DoctorID;
+            this.OperationTypeID = OperationTypeID;
+            this.DeadlineDate = DeadlineDate;
+            this.Priority = Priority;
+        }
+    }
+}
--- Domain/operationrequestmanagement/OperationRequestDto.cs
+++ Domain/operationrequestmanagement/OperationRequestDto.cs
@@ -5,7 +5,7 @@ namespace Hospital.Domain.operationrequestmanagement
 {
     public class OperationRequestDto
     {
-        public Guid ID { get; set; } // Unique identifier for the operation request
+        public OperationRequestId ID { get; set; } // Unique identifier for the operation request
         public Guid PatientID { get; set; } // Identifier for the patient linked to this operation request
         public LicenseNumber DoctorID { get; set; } // Identifier for the doctor who requested the operation
         public string OperationTypeID { get; set; } // Identifier for the type of operation to be performed
