6020939a2b3cd9a0712e92d7ae108fa994bbdc6b
ruivieira03 <121126024+ruivieira03@users.noreply.github.com>
2024-10-27 23:17:48 +0000

    Added Docs and tests

--- obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3171003c1c93bfa093a5e4ee18d24cf26727247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a51d433742a4a8263741e4b3979f92407fcbac")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
