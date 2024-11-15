8362cbf1bf00d6ff4e51b396556331ad558d3d31
Bernardo <tradestuga@gmail.com>
2024-11-15 15:44:29 +0000

    #49 Architecture Definition added: Clean Architecture

--- obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0297e8b89066d8feb499f7d61910eb41798aa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+424c2c31915dd98068b09b094bf145dfd0292252")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
