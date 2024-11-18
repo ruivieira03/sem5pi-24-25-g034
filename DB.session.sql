115e53e01f3c3484736b7349e68fcdb069363781
Bernardo <tradestuga@gmail.com>
2024-11-18 13:44:30 +0000

    #49 Data Schema added.
    DataSchema of Project added for Project. Though it needs to changed:
    Be added vallue objects , and column for every DomainClass

--- DB.session.sql
+++ DB.session.sql
@@ -1,1 +1,4 @@
 SELECT * from Patients
+
+
+
--- obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8362cbf1bf00d6ff4e51b396556331ad558d3d31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887ebbb0886b0f9766b94e9c6392247ccf85878c")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
