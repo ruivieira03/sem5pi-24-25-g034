9fbd33c0c32a0f6284abfae8c7011dd87ec04ea5
Rui Vieira <ruimiguelvieir@gmail.com>
2024-11-19 13:55:06 +0000

    merge fixes + added dependencies for the frontend react app

--- obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101197a1dcb2ee60de81e4cff49742a375e675df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13cead53d8ecf9df26ea2941cbc97b96136ddef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
