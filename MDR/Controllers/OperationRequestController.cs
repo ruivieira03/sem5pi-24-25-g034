3df50b5f048bb43d3f54a8bfac842181d7719795
1211523 <128061767+1211523@users.noreply.github.com>
2024-11-23 23:09:32 +0000

    #69 bugfixing: accounting for the possibility of gets returning nulls

--- MDR/Controllers/OperationRequestController.cs
+++ MDR/Controllers/OperationRequestController.cs
@@ -74,6 +74,12 @@ public class OperationRequestController : ControllerBase
     public async Task<ActionResult<OperationRequestDto>> GetById(Guid id)
     {
         var request = await _operationRequestService.GetByIdAsync(new OperationRequestId(id));
+
+        if (request == null)
+        {
+            return NotFound(); // Return 404 if request not found
+        }
+
         return Ok(request); // Return OK status with the request details
     }
 
@@ -83,6 +89,12 @@ public class OperationRequestController : ControllerBase
     public async Task<ActionResult<OperationRequestDto>> GetByPatient(Guid patientId)
     {
         var request = await _operationRequestService.GetOperationRequestsByPatientAsync(patientId);
+
+        if (request == null)
+        {
+            return NotFound(); // Return 404 if request not found
+        }
+
         return Ok(request); // Return OK status with the request details
     }
 
@@ -92,6 +104,12 @@ public class OperationRequestController : ControllerBase
     public async Task<ActionResult<OperationRequestDto>> GetByType(string operationTypeId)
     {
         var request = await _operationRequestService.GetOperationRequestsByTypeAsync(operationTypeId);
+
+        if (request == null)
+        {
+            return NotFound(); // Return 404 if request not found
+        }
+
         return Ok(request); // Return OK status with the request details
     }
 
@@ -101,6 +119,12 @@ public class OperationRequestController : ControllerBase
     public async Task<ActionResult<OperationRequestDto>> GetByPriority(int priority)
     {
         var request = await _operationRequestService.GetOperationRequestsByPriorityAsync(priority);
+
+        if (request == null)
+        {
+            return NotFound(); // Return 404 if request not found
+        }
+
         return Ok(request); // Return OK status with the request details
     }
 
@@ -111,6 +135,12 @@ public class OperationRequestController : ControllerBase
     public async Task<ActionResult<OperationRequestDto>> GetByStatus(string status)
     {
         var request = await _operationRequestService.GetOperationRequestsByStatusAsync(status);
+
+        if (request == null)
+        {
+            return NotFound(); // Return 404 if request not found
+        }
+
         return Ok(request); // Return OK status with the request details
     }
     */
--- MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff781ef41600d206d494ebf6892d29fc8a111a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75d741faecca68ad4d70e1500f436cd20793455")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
