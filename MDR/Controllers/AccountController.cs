78741f6a8c67b53c49f43c09f7927e1765e748a0
ruivieira03 <121126024+ruivieira03@users.noreply.github.com>
2024-11-23 17:57:13 +0000

    testing both workflows

--- MDR/Controllers/AccountController.cs
+++ MDR/Controllers/AccountController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
