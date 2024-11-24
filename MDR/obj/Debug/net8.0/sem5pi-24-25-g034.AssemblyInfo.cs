defb17517bc91ec8d641878ff772d9a8d1a47a4b
ruivieira03 <121126024+ruivieira03@users.noreply.github.com>
2024-11-24 21:28:24 +0000

    vm dployment test

--- MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40fca6feffd675d3e5e32b8e6209a05a096f7fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+173297dd8697b225f13a647dac0934217e24c607")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SPA/src/config.js
+++ SPA/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://localhost:5001";
+export const API_BASE_URL = "https://193.136.62.5:5001";
 // 10.9.22.94
