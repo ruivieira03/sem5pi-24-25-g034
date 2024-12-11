30f1d180eaf354ba71614916b1c9afc2547c4326
Bernardo <tradestuga@gmail.com>
2024-12-11 14:43:16 +0000

    #49 Small views  documentation upload
    Some views added for now , lacking others.

--- MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+067cce4ca29482b08de6f50ae15c0ac90874b39f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8dd00c130f4dd2361ec52abec998e240ba529d")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
