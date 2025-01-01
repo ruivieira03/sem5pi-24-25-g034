b6d800151977322f39bef7ae29dbb24c5bb758db
ruivieira03 <121126024+ruivieira03@users.noreply.github.com>
2025-01-01 15:51:15 +0000

    ManagerAllergies frontend update

--- MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be4fe5d2d3a1caa360ecc2e04fda9b94b132c7a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0345b5991ad837b0e1937b2dd84912a6f3959ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
