00874390fa9ae3e2b3387cff2ef4b1dbc8fe719f
Bernardo <tradestuga@gmail.com>
2025-01-01 15:56:14 +0000

    Saves Binary files changes commit

--- MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6a55daed1e8c5d89b812e270dcb296999f53ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07923209a85b8a948c47c572fe2b3ba763941552")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
