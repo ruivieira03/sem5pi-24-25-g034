ca82c77b7bfb2b0d90451e2e906d7483519d4040
Bernardo <tradestuga@gmail.com>
2025-01-05 21:58:08 +0000

    #110 #111 EnE and Unit Tests added  probably not added yet.

--- MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
+++ MDR/obj/Debug/net8.0/sem5pi-24-25-g034.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfb7368a8be91f9ad273ff13ec31488101f5191")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27416a840b93583119f757e4977fcf8148b3d338")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem5pi-24-25-g034")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
